350f4c0deba7d2769d1f1cd2c4c82324af0d4921
Jeremy <wangjeremy2000@gmail.com>
2019-09-14 10:21:09 -0400

    click

--- Clicker.py
+++ Clicker.py
@@ -2,20 +2,24 @@
 # WE WANT RED AND GREEN
 import pygame
 
-class clicker(pygame.sprite.Sprite, color, position, key):
+class Clicker:
 
-	def __init__(self, color, position, key):
+	def __init__(self, color, position, key, app):
 		#red = (255, 58, 44); green = (27, 239, 104)
 		#key = the button it should be mapped to
 		#pygame.K_LEFT, pygame.K_RIGHT
-		self.button = pygame.draw.circle(screen, color, position, 20)
+		self.position = position
+		self.key = key
+		self.app = app
+		self.color = color
 
 	def click(self):
+		self.button = pygame.draw.circle(self.app._display_surf, self.color, self.position, 20)
 		while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit();
-			if event.type == pygame.KEYDOWN:
-				if event.key == self.key:
-					pygame.draw.circle(screen, (27, 239, 104), (100, 100), 40)
-					#button is clicked. check for overlap
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit();
+				elif event.type == pygame.KEYDOWN:
+					if event.key == self.key:
+						pygame.draw.circle(screen, (27, 239, 104), position, 40)
+						#button is clicked. check for overlap
--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 import pygame
 import time
 from pygame.locals import *
+from Clicker import Clicker
 from Column import Column
 from Note import Note
  
@@ -12,21 +13,15 @@ class App:
         self._running = True
         self._display_surf = None
         self.size = self.width, self.height = 640, 400
-        
         self.background_colour = (255,255,255)
-        
-
  
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-      
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)      
         self._display_surf.fill((255,255,255))
         self.col = Column(int(self.width/3), self)
-        self.ol = Column(int(2*self.width/3), self)
-        
-        
-        self._running = True
+        self.ol = Column(int(2*self.width/3), self)   
+
  
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -39,6 +34,7 @@ class App:
         self._display_surf.fill(self.background_colour)
         self.col.move()
         self.ol.move()
+        self.leftbutton = pygame.draw.circle(self._display_surf, (255, 58, 44), (100, 100), 20)
         time.sleep(self.t)
         pygame.display.flip()
         
