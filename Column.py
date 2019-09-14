5c7ac09bc276cebdbbf3f712fe69641f8f833a55
Catherine Lee <cathyslee0@gmail.com>
2019-09-14 09:49:43 -0400

    some basic display stuff

--- Column.py
+++ Column.py
@@ -1,2 +1,21 @@
 # RNG for which column
 # RNG for spawn rate
+from Note import Note
+
+
+class Column:
+    def __init__(self, center, app):
+        self.app = app
+        self.center = center
+        self.notes = []
+        note = Note(self.app, self.center)
+        self.notes.append(note)
+
+    def move(self):
+        for note in self.notes:
+            note.move()
+
+    def newNote(self):
+        note = Note(self.app, self.center)
+        self.notes.append(note)
+        
--- Note.py
+++ Note.py
@@ -1,2 +1,22 @@
 # The things that fall
 # WE WANT YELLOW
+import pygame
+
+
+class Note:
+
+    def __init__(self, app, center):
+        self.app = app
+        self.ypos = 0
+        self.center = center
+        self.note = pygame.draw.circle(app._display_surf, (0,0,255), (center, 200), 40, 0)
+        
+    def move(self):
+        pygame.draw.circle(self.app._display_surf, (0,0,255), (self.center, self.ypos), 40, 0)
+        self.ypos += 1
+
+
+
+        
+
+
